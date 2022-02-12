7bafac12b4e8f2e75aee81f9cff46dd3f027a849
DTPrendergast <DTPrendergast@users.noreply.github.com>
2022-02-11 17:18:11 -0700

    import data

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -12,15 +12,29 @@ sys.path.insert(0, '../libs/')
 import csv
 import copy
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 # Global variables
 RESOURCES_DIR = '/resources/'
-FN_RESPONSES = RESOURCES_DIR + 'sceneID_LP_and_response.csv'
-FN_SCENE_DATA = RESOURCES_DIR + 'Study3_SceneData.csv'
+RESPONSES_FN = RESOURCES_DIR + 'sceneID_LP_and_response.csv'
+SCENE_DATA_FN = RESOURCES_DIR + 'Study3_SceneData.csv'
 
 
 def main():
+    responses = pd.read_csv(RESPONSES_FN)
+    responses.head()
+
+
+
+def load_data(fn):
+    data = []
+    with open(fn,'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            data.append(row)
+        csvfile.close()
+    return data
 
 
 if __name__ == '__main__':
