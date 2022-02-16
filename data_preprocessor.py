a8afb20ce55bb6e70978686fc1e50e910b42ac14
DTPrendergast <DTPrendergast@users.noreply.github.com>
2022-02-15 19:09:11 -0700

    calculating line of sight vectors

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -22,39 +22,76 @@ RESOURCES_DIR = 'resources/'
 RESPONSES_FN = RESOURCES_DIR + 'sceneID_LP_and_response.csv'
 SCENE_DATA_FN = RESOURCES_DIR + 'Study3_SceneData.csv'
 TRAINING_EXAMPLES_FN = RESOURCES_DIR + 'training_examples.csv'
-NUM_SCENES = 84
+
 ROTATION_ANGLES = np.linspace(0, 2*np.pi, num=13)[1:-1]
-LP_LIST = ['above', 'against', 'behind', 'below', 'beside', 'close', 'front', 'left', 'near', 'next', 'on', 'on top', 'over', 'right']
+# NUM_SCENES = 84
+# LP_LIST = ['above', 'against', 'behind', 'below', 'beside', 'close', 'front', 'left', 'near', 'next', 'on', 'on top', 'over', 'right']
+NUM_SCENES = 4
+SCENE_LIST = list(range(1, NUM_SCENES + 1))
+LP_LIST = ['above', 'against', 'behind']
 
 TRNG_EXAMPLE_HEADERS = ['Vector to Target X', 'Vector to Target Y', 'Vector to Target Z', 'Vector to Reference X', 'Vector to Reference Y', 'Vector to Reference Z', 'LP Scores Vector']
 
+# Set pandas options
+# pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_columns', None)
 
 def main():
     responses = pd.read_csv(RESPONSES_FN)
-    print(responses.head())
-    print(responses.dtypes)
-    print(responses.index)
-    print(responses.columns)
-    scene_1_responses = responses[(responses['SceneID'] == 1) and (responses['Locative Preposition'] == 'above')]
-    print(scene_1_responses.head())
-    print(scene_1_responses.count)
+    num_responses = responses.shape[0]
+    print('Responses DF Columns:  ', responses.columns)
 
     scene_data = pd.read_csv(SCENE_DATA_FN)
     print(scene_data.head())
-    print(scene_data.dtypes)
-    print(scene_data.index)
-    print(scene_data.columns)
+    print('Scene Data DF Columns:  ', scene_data.columns)
+    print('Test: ', scene_data[scene_data['SceneID'] == 1]['Cube-X'])
+
+    #  Calculate LoS vectors from camera to objects
+    los_vectors = pd.DataFrame(columns=['SceneID', 'LoS_to_Tgt', 'LoS_to_Ref'])
+    los_vectors['SceneID'] = SCENE_LIST
+    for scene in SCENE_LIST:
+        cam_loc, tgt_loc, ref_loc = get_point_locations(scene_data, scene)
+        los_to_tgt = np.subtract(tgt_loc, cam_loc)
+        los_to_ref = np.subtract(ref_loc, cam_loc)
+        los_vectors[los_vectors['SceneID'] == scene]['LoS_to_Tgt'] = los_to_tgt
+        los_vectors[los_vectors['SceneID'] == scene]['LoS_to_Ref'] = los_to_ref
+
+    print(los_vectors.head())
 
     #  Build Training Examples
+    features = []
+    scores = []
     trng_examples = pd.DataFrame(columns=TRNG_EXAMPLE_HEADERS)
-    for scene in list(range(1, NUM_SCENES + 1)):
+    for scene in SCENE_LIST:
         for lp in LP_LIST:
-            temp_df = responses[(responses['SceneID'] == scene) and (responses['Locative Preposition'] == lp)]
+            temp_df = responses[(responses['SceneID'] == scene) & (responses['Locative Preposition'] == lp)]
+            # print(temp_df.head())
+            num_rows = temp_df.shape[0]
+            temp_features = np.zeros((num_rows, 6), dtype=np.float64)
+
+            print(temp_df.shape)
+
 
 
     #  Scale Features
 
     #  Data Augmentation using Rotation
+
+
+def get_point_locations(scene_data, scene):
+    cam_x = scene_data[scene_data['SceneID'] == scene]['Camera Location X'].iloc[0]
+    cam_y = scene_data[scene_data['SceneID'] == scene]['Camera Location Y'].iloc[0]
+    cam_z = scene_data[scene_data['SceneID'] == scene]['Camera Location Z'].iloc[0]
+    tgt_x = scene_data[scene_data['SceneID'] == scene]['Sphere-X'].iloc[0]
+    tgt_y = scene_data[scene_data['SceneID'] == scene]['Sphere-Y'].iloc[0]
+    tgt_z = scene_data[scene_data['SceneID'] == scene]['Sphere-Z'].iloc[0]
+    ref_x = scene_data[scene_data['SceneID'] == scene]['Cube-X'].iloc[0]
+    ref_y = scene_data[scene_data['SceneID'] == scene]['Cube-Y'].iloc[0]
+    ref_z = scene_data[scene_data['SceneID'] == scene]['Cube-Z'].iloc[0]
+    cam_loc = [cam_x, cam_y, cam_z]
+    tgt_loc = [tgt_x, tgt_y, tgt_z]
+    ref_loc = [ref_x, ref_y, ref_z]
+    return cam_loc, tgt_loc, ref_loc
 
 def rotate_point_about_z(pt, alpha):
     #  pt is array or tuple of floats
